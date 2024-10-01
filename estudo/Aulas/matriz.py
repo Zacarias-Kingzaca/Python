15e6814d4e9daf82e12d29a7e100097528d15695
Zacarias-Kingzaca <zacariaseduardoj@gmail.com>
2024-10-01 23:53:13 +0100

    estava estudando

---
+++ estudo/Aulas/matriz.py
@@ -0,0 +1,16 @@
+from os import system
+system ("cls")
+print() 
+
+print(50*"*")
+print("Trabalhando com Matriz em python")
+print(50*"*")
+
+matriz =  ([ [1 ,2 , 3],[4, 5, 6]])
+print(matriz)
+
+elemento = matriz[0]
+print(elemento)
+print(50*"*")
+print("Finish")
+print(50*"*")
