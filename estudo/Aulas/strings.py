fe9b01d691c78344daa2caf2221d98de7c213bae
Zacarias-Kingzaca <zacariaseduardoj@gmail.com>
2024-10-03 22:57:38 +0100

    estava estudando

---
+++ estudo/Aulas/strings.py
@@ -0,0 +1,16 @@
+from os import system
+system ("cls")
+print()
+
+
+print(20*"-")
+print("Trabalhando com strings")
+print(20*"-")
+
+
+nome = "ZACARIAS"
+sobre_nome = "KINGZACA"
+nome_comleto = nome + " " + "" + sobre_nome
+
+print(nome_comleto.lower())
+print(nome_comleto.upper())
