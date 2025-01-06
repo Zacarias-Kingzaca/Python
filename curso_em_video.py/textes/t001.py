69f22509933dbd48b7de46ffab05ad7eed7b6d47
Zacarias-Kingzaca <zacariaseduardoj@gmail.com>
2025-01-06 09:33:58 +0100

    novo

---
+++ curso_em_video.py/textes/t001.py
@@ -0,0 +1,14 @@
+from os import system
+import random
+system("cls")
+print()
+
+
+print(20*"=")
+print("Seja Bem-Vindo")
+print(20*"=")
+
+
+n1 = input("Digita o nome do primeiro aluno :")
+n2 = input("Ditgita o nome do segundo aluno :")
+
