05ededdd6c0f9f0fdceeafa62440ab62735c67e3
Zacarias King Zaca <zacariaseduardoj@gmail.com>
2025-04-26 10:12:11 +0100

    novo

--- curso_em_video.py/textes/t78.py
+++ curso_em_video.py/textes/t78.py
@@ -1,2 +1,8 @@
 from os import system
-sys
+system("cls")
+print()
+
+custo = float(input("Digita o custo do produto kzs: "))
+preco = float(input("Digita o preço do produto kzs: "))
+
+print("VENDA SEM PREJUÍZO" if cust)
