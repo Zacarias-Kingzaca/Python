ac69fd08d658c7624d36ad522da68881cb059c08
Zacarias King Zaca <181795029+Zacarias-Kingzaca@users.noreply.github.com>
2025-11-18 15:48:35 +0000

    novo

---
+++ estudo/paralelos/ex93.py
@@ -0,0 +1,19 @@
+from os import system
+import csv
+system("cls")
+print()
+
+
+with open("fixa.csv", "w", newline="", encoding="UTF-8") as f:
+    arquivo = csv.writer(f)
+    arquivo.writerow([[f"{"Nome":<12} {"Idade":<15} {"Nota"} {"Média":>15} "]])
+    arquivo.writerow([[f"{"Nome":<10} {"Preço":<10}"]])
+    arquivo.writerow([[f"{"peixe":<10} {200:>10.2f}"]])
+        
+with open("fixa.csv", "r", encoding="utf-8") as f:
+    arquivo = csv.reader(f)
+    print("=" * 62)
+    print(f"{"LISTA DE PRODUTOS":^62}")
+    print("=" * 62)
+    for p in arquivo:
+        print(p)
